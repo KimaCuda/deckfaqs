1e2c3d871f047d043cb65f1018ab59367471d484
Jariullah Safi <safijari@isu.edu>
2024-04-04 23:56:11 +0200

    fronen

--- rollup.config.js
+++ rollup.config.js
@@ -23,13 +23,13 @@ export default defineConfig({
         }),
     ],
     context: 'window',
-    external: ['react', 'react-dom', 'decky-fronend-lib'],
+    external: ['react', 'react-dom', 'decky-frontend-lib'],
     output: {
         file: 'dist/index.js',
         globals: {
             react: 'SP_REACT',
             'react-dom': 'SP_REACTDOM',
-	    'decky-fronend-lib': 'DFL',
+	    'decky-frontend-lib': 'DFL',
         },
         format: 'iife',
         exports: 'default',
